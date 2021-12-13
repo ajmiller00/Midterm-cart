bf80215e7ed5c48b2da5ad4dc1f11e890018fc92
haijun12 <79031887+haijun12@users.noreply.github.com>
2021-12-13 10:53:13 -0600

    Update cart.js

--- cart.js
+++ cart.js
@@ -53,7 +53,7 @@ app.get('', (req, res) => {
 				res.write("</tr>");
 				var cart = result.cart;
 			    	var overallTotal = 0;
-				for (i = 0; i < cart.length; i++)
+				for (i = 1; i < cart.length; i++)
 				{
 					var item = cart[i].cart_item;
 					var quantity = cart[i].cart_quantity;
@@ -132,14 +132,14 @@ app.post('/checkout', (req, res) => {
 		var query = {
 			email : currUser.email
         }
-//         user.findOne(
-//             query,
-//             { $set: {cart: {
-//                 car_item: "hello",
-//                 cart_quantity: 10,
-//                 cart_price: 0
-//             }}}
-//         )
+        user.findOne(
+            query,
+            { $set: {cart: {
+                car_item: "hello",
+                cart_quantity: 10,
+                cart_price: 0
+            }}}
+        )
         })
     })
 })
