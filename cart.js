7befeb60150f43f17b7c16be1f014c3b7345455e
haijun12 <79031887+haijun12@users.noreply.github.com>
2021-12-13 18:46:25 -0600

    Update cart.js

--- cart.js
+++ cart.js
@@ -31,7 +31,8 @@ app.get('', (req, res) => {
             {
                 res.write("<div id = 'cart' style = \"font-size:30px; font-family: 'Josefin Sans', sans-serif;\">")
                 res.write("User is not logged in<br> <br><br><br>");
-                res.write("</div>")
+                res.write("</div>");
+		res.write("<footer>&copy; 2021 Rêve au Chocolat – 23 Fausse Street, Cambridge, MA – (617) 555 0113</footer>");
 		db.close();
 		res.end();
             } else {
